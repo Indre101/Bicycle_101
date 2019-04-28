8d815682117b67cb9b8d034a6e18bc19ee28a1ce
indre101 <zygaityte.indre@gmail.com>
2019-04-28 20:48:12 +0200

    made bycycle animation

--- js/script.js
+++ js/script.js
@@ -1,10 +1,21 @@
 const body = document.querySelector("body");
-const bicycleContainer = document.querySelector(".bicycleContainer");
+const canvas = document.querySelector("canvas");
 
-const bicycle = document.createElement("div");
-bicycle.classList.add("bicycleBG");
+const c = canvas.getContext("2d");
 
-bicycleContainer.width = window.innerWidth;
+const bicycle = new Image();
+bicycle.src = "../Icons/BlÜ/4__Cykel_BlÜ_1.png";
+
+// context.drawImage(img,sx,sy,swidth,sheight,x,y,width,height);
+
+c.drawImage(bicycle, 100, 100, 300, 300, 300);
+
+
+
+// const bicycle = document.createElement("div");
+// bicycle.classList.add("bicycleBG");
+
+// bicycleContainer.width = window.innerWidth;
 
 
 // console.log(window.getPropertyValue("innerWidth"));
@@ -20,48 +31,46 @@ bicycleContainer.width = window.innerWidth;
 // }
 
 
-const test = document.querySelector(".test");
-test.style.left = (Math.random() * 90) + "vw";
-test.style.top = (Math.random() * 90) + "vh";
+// const test = document.querySelector(".test");
+// test.style.left = (Math.random() * 90) + "vw";
+// test.style.top = (Math.random() * 90) + "vh";
 
-let x = (Math.random() * 3);
-let y = (Math.random() * 3);
-
-
-
-
-animate()
-
-function animate() {
-  requestAnimationFrame(animate)
-
-  let bgSize = 100
-
-
-  let leftVal = parseInt(test.style.left, 10);
-  test.style.left = (leftVal + x) + "vw";
-  leftVal = parseInt(test.style.left, 10);
-
-
-  // let topVal = parseInt(test.style.top, 10);
-  // test.style.top = -((topVal + y) + "vh");
-  // topVal = parseInt(test.style.top, 10)
-
-  if (leftVal + bgSize > bicycleContainer.innerWidth || leftVal - bgSize < 0) {
-    test.style.left = -((leftVal + x) + "vw");
-
-    console.log(leftVal)
-
-  }
-  // else if (topVal + bgSize > window.innerHeight || topVal - bgSize < 0) {
-  //   topVal = -topVal;
-  //   console.log("topVal")
-
-  // }
+// let x = (Math.random() * 3);
+// let y = (Math.random() * 3);
 
 
 
 
+// animate()
+
+// function animate() {
+//   requestAnimationFrame(animate)
+
+//   let bgSize = 100
 
 
-}
+//   let leftVal = parseInt(test.style.left, 10);
+//   test.style.left = (leftVal + x) + "vw";
+//   leftVal = parseInt(test.style.left, 10);
+
+
+// let topVal = parseInt(test.style.top, 10);
+// test.style.top = -((topVal + y) + "vh");
+// topVal = parseInt(test.style.top, 10)
+
+// if (leftVal + bgSize > bicycleContainer.innerWidth || leftVal - bgSize < 0) {
+//   test.style.left = -((leftVal + x) + "vw");
+
+//   console.log(leftVal)
+
+// }
+// else if (topVal + bgSize > window.innerHeight || topVal - bgSize < 0) {
+//   topVal = -topVal;
+//   console.log("topVal")
+
+// }
+
+// }
+
+// bicycleContainer.width = window.innerWidth;
+// bicycleContainer.height = window.innerHeight;
---
+++ test/script.js
@@ -0,0 +1,83 @@
+const canvas = document.querySelector("canvas");
+const ctx = canvas.getContext("2d");
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+let bicycle = new Image();
+bicycle.src = "../Icons/BLUE/bicycle.png";
+
+
+
+function NewImage(bicycle, x, y, dx, dy, width, height) {
+  this.bicycle = bicycle;
+  this.x = x;
+  this.y = y;
+  this.dx = dx;
+  this.dy = dy;
+  this.width = width;
+  this.height = height;
+
+  this.draw = function () {
+
+    ctx.drawImage(this.bicycle, this.x, this.y, this.width, this.height)
+  }
+
+  this.update = function () {
+
+    if (this.x + this.width > window.innerWidth || this.x - this.width < 0) {
+
+      this.dx = -this.dx
+    } else if (this.y + this.height || this.y - this.height < 0) {
+      this.dy = -this.dy;
+
+    }
+
+    this.y += this.dy;
+
+    this.x += this.dx;
+
+  }
+
+
+
+
+}
+
+// array for images
+
+let bicycleArray = []
+
+for (let index = 0; index < 5; index++) {
+  let x = Math.random() * window.innerWidth;
+  let y = Math.random() * window.innerHeight;
+  let dx = (Math.random() - 0.5) + Math.random() * 2;
+  let dy = (Math.random() - 0.5) + Math.random() * 2;
+  let width = 100;
+  let height = 100;
+  bicycleArray.push(new NewImage(bicycle, x, y, dx, dy, width, height));
+
+}
+
+// function for moving images
+
+// animate()
+
+window.onload = function (params) {
+  animate()
+  console.log(bicycleArray)
+
+}
+
+function animate() {
+  requestAnimationFrame(animate);
+  ctx.clearRect(0, 0, innerWidth, innerHeight);
+
+  bicycleArray.forEach((e) => {
+    e.draw();
+    e.update();
+
+  })
+
+
+}
